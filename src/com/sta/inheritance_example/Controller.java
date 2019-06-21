44b6940e859baf10afbc4f478fc05d62398caa9f
polherron <pol.herron@gmail.com>
2019-06-21 15:04:59 +0100

    Object Lesson

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -1,5 +1,7 @@
 package com.sta.inheritance_example;
 
+import java.util.ArrayList;
+
 public class Controller {
 
 		public static void main(String[] args) {
@@ -17,7 +19,38 @@ public class Controller {
 			myPooch.voice();
 			System.out.println("Purchase Price is " + myPooch1.purchasePrice);
 			System.out.println("Purchase Price is " + myPooch1.sellingPrice);
-
+			
+			Object dogObject = new Dog();
+			//We can check to see if the dogObject is an 
+			//instance of a dog type, if it is we can cast
+			//and get access to all of the dog contents.
+			if(dogObject instanceof Dog)
+			{
+				double price = ((Dog) dogObject).purchasePrice;
+			}
+			
+			ArrayList<Object> myDogs = new ArrayList<Object>();
+			myDogs.add(myDog);
+			myDogs.add(myPooch);
+			myDogs.add(myPooch1);
+			myDogs.add(dogObject);
+			
+			for (Object dog : myDogs) {
+				if(dog instanceof Poodle)
+				{
+					System.out.println("I'm a poodle");
+					((Poodle)dog).voice();
+				}
+				else if(dog instanceof Dog)
+				{
+					System.out.println("I'm a proper dog");
+					((Dog)dog).voice();
+				}
+			}
+			
+			Poodle mySuperPoodle = new Poodle();
+			mySuperPoodle.voice();
+					
 		}
 
 	}
