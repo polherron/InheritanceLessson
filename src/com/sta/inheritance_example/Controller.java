cdb7101006f2d08695c9ca4c5d5cc2f7bcbb40d6
polherron <pol.herron@gmail.com>
2019-06-22 16:19:35 +0100

    Sorting a collection of complex objects

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -1,29 +1,37 @@
 package com.sta.inheritance_example;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ArrayList;
 
 public class Controller {
 
-		public static void main(String[] args) {
-			Dog myDog = new Dog(true, "Star", 200, 1);
-			Poodle myPooch = new Poodle(true, "Moon", 200, 2);
-			Dog myPooch1 = new Poodle(true, "Venus", 200, 3);
-			Object o = new Poodle(true, "Mars", 200, 4);
-			ArrayList<Object> myDoggies = new ArrayList<Object>();
-			
-			myDoggies.add(myDog);
-			myDoggies.add(myPooch);
-			myDoggies.add(myPooch1);
-			myDoggies.add(o);
-			
-			for (Object dog : myDoggies) {
-				System.out.println(dog.getName);
+	public static void main(String[] args) {
+		Dog myDog = new Dog(true, "Star", 200, 1);
+		Poodle myPooch = new Poodle(true, "Moon", 200, 2);
+		Dog myPooch1 = new Poodle(true, "Venus", 200, 3);
+		ArrayList<Pet> myPets = new ArrayList<Pet>();
+		myPets.add(myDog);
+		myPets.add(myPooch);
+		myPets.add(myPooch1);
+
+		for (Pet pet : myPets) {
+			System.out.println(pet.getName());
+		}
+
+		Collections.sort(myPets, new Comparator<Pet>() {
+			public int compare(Pet pet1, Pet pet2) {
+				return Integer.valueOf(pet1.getName().compareTo(pet2.getName()));
 			}
-						
-			
-			}
+		});
+
+		System.out.println("Sorted");
+
+		for (Pet pet : myPets) {
+			System.out.println(pet.getName());
+
 		}
 
 	}
-}
 
+}
