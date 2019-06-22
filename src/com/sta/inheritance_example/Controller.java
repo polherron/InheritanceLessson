813b9e76204c2d55a8f03b6cfcb58ddf6c885a0f
polherron <pol.herron@gmail.com>
2019-06-22 16:47:20 +0100

    Helper Methods

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -15,10 +15,65 @@ public class Controller {
 		myPets.add(myPooch);
 		myPets.add(myPooch1);
 
+		sortArray(myPets);
+		printListArrayNames(myPets);
+		
+		String searchString = "Star";
+		
+		removeElement(myPets, searchString);
+		printListArrayNames(myPets);
+
+	}
+
+	/**
+	 * @param myPets
+	 * @param searchString
+	 */
+	private static void removeElement(ArrayList<Pet> myPets, String searchString) {
+		if(elementExistsByName(myPets, searchString))
+		{
+			int index = findElementByName(myPets, searchString);
+			myPets.remove(index);
+		}
+	}
+
+	/**
+	 * @Prints Names of Pets
+	 */
+	private static void printListArrayNames(ArrayList<Pet> myPets) {
 		for (Pet pet : myPets) {
 			System.out.println(pet.getName());
 		}
+	}
 
+	//Checks if element exists
+	private static boolean elementExistsByName(ArrayList<Pet> myPets, String searchString) {
+		for (int i = 0; i < myPets.size(); i++) {
+			if (myPets.get(i).getName().contains(searchString))
+				return true;
+		}
+		return false;
+	}
+
+	//Returns the index number of an ArrayList element using Name
+	private static int findElementByName(ArrayList<Pet> myPets, String searchString) {
+		int index = 0;
+		for (int i = 0; i < myPets.size(); i++) {
+			if (myPets.get(i).getName().contains(searchString))
+			{
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	/**
+	 * Sorts the array by pet Name
+	 */
+	private static void sortArray(ArrayList<Pet> myPets) {
+		printListArrayNames(myPets);
+
+		//Using the collections pattern to sort ListArray
 		Collections.sort(myPets, new Comparator<Pet>() {
 			public int compare(Pet pet1, Pet pet2) {
 				return Integer.valueOf(pet1.getName().compareTo(pet2.getName()));
@@ -27,11 +82,7 @@ public class Controller {
 
 		System.out.println("Sorted");
 
-		for (Pet pet : myPets) {
-			System.out.println(pet.getName());
-
-		}
-
+		printListArrayNames(myPets);
 	}
 
 }
