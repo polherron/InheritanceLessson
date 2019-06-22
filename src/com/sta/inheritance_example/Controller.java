0307265e6ab014030795e52d7482f13586911d18
polherron <pol.herron@gmail.com>
2019-06-22 14:35:12 +0100

    Update

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -1,5 +1,7 @@
 package com.sta.inheritance_example;
 
+import java.util.ArrayList;
+
 public class Controller {
 
 		public static void main(String[] args) {
@@ -17,8 +19,30 @@ public class Controller {
 			myPooch.voice();
 			System.out.println("Purchase Price is " + myPooch1.purchasePrice);
 			System.out.println("Purchase Price is " + myPooch1.sellingPrice);
+			
+			Object o = new Poodle(200);
+			
+			ArrayList<Object> myDoggies = new ArrayList<Object>();
+			
+			myDoggies.add(myDog);
+			myDoggies.add(myPooch);
+			myDoggies.add(myPooch1);
+			myDoggies.add(o);
+			
+			for (Object dog : myDoggies) {
+				if (dog instanceof Poodle ) {
+					System.out.println("I'm a poodle");
+					((Poodle)dog).voice();
+					
+				} else {
+					if (dog instanceof Dog ) {
+						System.out.println("I'm a Dog");
+						((Dog)dog).voice();
+				}
 
+			}
 		}
 
 	}
+}
 
