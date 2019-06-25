e6da01f0a20800cb50f27eeeb282696525fd5f39
polherron <pol.herron@gmail.com>
2019-06-25 14:25:55 +0100

    Add bubble sort

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -8,24 +8,31 @@ public class Controller {
 
 	public static void main(String[] args) {
 		Dog myDog = new Dog(true, "Star", 200, 1);
+		Utilities.doSomething(myDog);
 		Poodle myPooch = new Poodle(true, "Moon", 200, 2);
 		Dog myPooch1 = new Poodle(true, "Venus", 200, 3);
 		ArrayList<Pet> myPets = new ArrayList<Pet>();
 		myPets.add(myDog);
 		myPets.add(myPooch);
 		myPets.add(myPooch1);
+		
+		int a = 5;
+		int b = 6;
+		int c = Utilities.add(a, b);
 
 		for (Pet pet : myPets) {
 			System.out.println(pet.getName());
 		}
-
-		Collections.sort(myPets, new Comparator<Pet>() {
+		
+		Sort.Sort(myPets);
+		/*Collections.sort(myPets, new Comparator<Pet>() {
 			public int compare(Pet pet1, Pet pet2) {
 				return Integer.valueOf(pet1.getName().compareTo(pet2.getName()));
 			}
 		});
-
-		System.out.println("Sorted");
+		*/
+		
+				System.out.println("Sorted");
 
 		for (Pet pet : myPets) {
 			System.out.println(pet.getName());
