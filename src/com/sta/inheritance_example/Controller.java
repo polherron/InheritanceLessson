e284043da8856298f49fe93f1c64c9e7dfe18f26
polherron <pol.herron@gmail.com>
2019-06-25 11:22:29 +0100

    Added extra dog object

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -21,10 +21,23 @@ public class Controller {
 			System.out.println("Purchase Price is " + myPooch1.purchasePrice);
 			System.out.println("Purchase Price is " + myPooch1.sellingPrice);
 			
+			Object myDog1 = new Dog(200);
+			myDog.voice();
+			System.out.println("Purchase Price is " + myDog.purchasePrice);
+			System.out.println("Purchase Price is " + myDog.sellingPrice);
+			
 			Object o = new Poodle(200);
 			if (o instanceof Poodle) {
+				System.out.println("calling object type");
 				System.out.println("Purchase Price is " + ((Poodle)o).purchasePrice);
 				System.out.println("Purchase Price is " + ((Poodle)o).sellingPrice);
+			}
+			
+			Object myDog2 = new Dog(200);
+			if (myDog2 instanceof Poodle) {
+				System.out.println("calling object type");
+				System.out.println("Purchase Price is " + ((Dog)o).purchasePrice);
+				System.out.println("Purchase Price is " + ((Dog)o).sellingPrice);
 			}
 		}
 }
