de467b66df3af661cd16e0a60ca413f1c3eb78ac
polherron <pol.herron@gmail.com>
2019-06-25 10:59:37 +0100

    updated doggy price

--- src/com/sta/inheritance_example/Controller.java
+++ src/com/sta/inheritance_example/Controller.java
@@ -4,7 +4,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		
-		Dog myDog = new Dog(200);
+		Dog myDog = new Dog(50);
 		myDog.voice();
 		System.out.println("Purchase Price is " + myDog.purchasePrice);
 		System.out.println("Selling Price is " + myDog.sellingPrice);
